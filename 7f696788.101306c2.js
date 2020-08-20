986d65afc074e6d3228bc82117472745093888a0
Quinten Steenhuis <qsteenhuis@gmail.com>
2020-08-19 21:03:29 -0400

    Deploy website - based on 0ec3623cb84a9f6f19aac05b25e66566d273be30

--- 7f696788.101306c2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{145:function(s){s.exports=JSON.parse('{"allTagsPath":"/legal-tech-class/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/legal-tech-class/blog/tags/docusaurus"}')}}]);
--- 993f539b.766e9551.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{151:function(a){a.exports=JSON.parse('{"allTagsPath":"/legal-tech-class/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/legal-tech-class/blog/tags/facebook"}')}}]);
