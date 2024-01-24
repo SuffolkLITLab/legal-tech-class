fcbf3e0f3d0d01b46abc65e1f8c36634ce4aa93d
Quinten Steenhuis <qsteenhuis@gmail.com>
2024-01-24 13:27:48 -0500

    Finish upgrade to 3.1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -141,19 +141,19 @@ module.exports = {
     // },
   ],
   themes: [
-    // ... Your other themes.
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      {
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
-        hashed: true,
-        // For Docs using Chinese, The `language` is recommended to set to:
-        // ```
-        // language: ["en", "zh"],
-        // ```
-      },
+      // ... Your other themes.
+      [
+        require.resolve("@easyops-cn/docusaurus-search-local"),
+        /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+        ({
+          // ... Your options.
+          // `hashed` is recommended as long-term-cache of index file is possible.
+          hashed: true,
+          // For Docs using Chinese, The `language` is recommended to set to:
+          // ```
+          // language: ["en", "zh"],
+          // ```
+        }),
+      ],
     ],
-  ],
-
 };
